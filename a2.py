baf86af77f85ec6b3a57b731cb2979f1a473be9b
dangviet <dangviet@ualberta.ca>
2024-10-06 15:41:49 -0600

    Update 3/10

--- a2.py
+++ a2.py
@@ -2,34 +2,42 @@
 # Implement the specified commands to complete the assignment
 # Full assignment specification here: https://webdocs.cs.ualberta.ca/~mmueller/courses/cmput455/assignments/a2.html
 
-import sys
 import random
+import sys
 import time
+import cProfile
+
 
 class CommandInterface:
 
     def __init__(self):
         # Define the string to function command mapping
         self.command_dict = {
-            "help" : self.help,
-            "game" : self.game,
-            "show" : self.show,
-            "play" : self.play,
-            "legal" : self.legal,
-            "genmove" : self.genmove,
-            "winner" : self.winner,
-            "timelimit" : self.timelimit,
-            "solve" : self.solve
+            "help": self.help,
+            "game": self.game,
+            "show": self.show,
+            "play": self.play,
+            "legal": self.legal,
+            "genmove": self.genmove,
+            "winner": self.winner,
+            "timelimit": self.timelimit,
+            "solve": self.solve
         }
         self.board = [[None]]
         self.player = 1
         self.start_time = None
         self.time_limit = 1.0
         self.timed_out = False
-    
-    #===============================================================================================
+        self.positions = {}
+        self.row_number = 0
+        self.column_number = 0
+        self.half_row_number_threshold = 0
+        self.half_column_number_threshold = 0
+        self.number_of_calls = 0
+
+    # ===============================================================================================
     # VVVVVVVVVV START of PREDEFINED FUNCTIONS. DO NOT MODIFY. VVVVVVVVVV
-    #===============================================================================================
+    # ===============================================================================================
 
     # Convert a raw string to a command and a list of arguments
     def process_command(self, str):
@@ -47,7 +55,7 @@ class CommandInterface:
             print(e, file=sys.stderr)
             print("= -1\n")
             return False
-        
+
     # Will continuously receive and execute commands
     # Commands should return True on success, and False on failure
     # Every command will print '= 1' or '= -1' at the end of execution to indicate success or failure respectively
@@ -75,7 +83,8 @@ class CommandInterface:
             try:
                 converted_args.append(int(arg))
             except ValueError:
-                print("Argument '" + arg + "' cannot be interpreted as a number.\nExpected arguments:", template, file=sys.stderr)
+                print("Argument '" + arg + "' cannot be interpreted as a number.\nExpected arguments:", template,
+                      file=sys.stderr)
                 return False
         args = converted_args
         return True
@@ -88,13 +97,13 @@ class CommandInterface:
         print("exit")
         return True
 
-    #===============================================================================================
+    # ===============================================================================================
     # ɅɅɅɅɅɅɅɅɅɅ END OF PREDEFINED FUNCTIONS. ɅɅɅɅɅɅɅɅɅɅ
-    #===============================================================================================
+    # ===============================================================================================
 
-    #===============================================================================================
+    # ===============================================================================================
     # VVVVVVVVVV START OF ASSIGNMENT 2 FUNCTIONS. ADD/REMOVE/MODIFY AS NEEDED. VVVVVVVV
-    #===============================================================================================
+    # ===============================================================================================
 
     def game(self, args):
         if not self.arg_check(args, "n m"):
@@ -103,17 +112,23 @@ class CommandInterface:
         if n < 0 or m < 0:
             print("Invalid board size:", n, m, file=sys.stderr)
             return False
-        
+
         self.board = []
         for i in range(m):
-            self.board.append([None]*n)
+            self.board.append([None] * n)
         self.player = 1
 
         # Set the default time limit to 1
         self.timelimit(["1"])
 
+        # Set the row number, column number, half row number threshold and half column number threshold
+        self.row_number = n
+        self.column_number = m
+        self.half_column_number_threshold = self.column_number // 2 + self.column_number % 2
+        self.half_row_number_threshold = self.row_number // 2 + self.row_number % 2
+
         return True
-    
+
     def show(self, args):
         for row in self.board:
             for x in row:
@@ -121,17 +136,17 @@ class CommandInterface:
                     print(".", end="")
                 else:
                     print(x, end="")
-            print()                    
+            print()
         return True
 
     def is_legal_reason(self, x, y, num):
         if self.board[y][x] is not None:
             return False, "occupied"
-        
+
         consecutive = 0
         count = 0
         self.board[y][x] = num
-        for row in range(len(self.board)):
+        for row in range(self.column_number):
             if self.board[row][x] == num:
                 count += 1
                 consecutive += 1
@@ -140,11 +155,13 @@ class CommandInterface:
                     return False, "three in a row"
             else:
                 consecutive = 0
-        too_many = count > len(self.board) // 2 + len(self.board) % 2
-        
+        if count > self.half_column_number_threshold:
+            self.board[y][x] = None
+            return False, "too many " + str(num)
+
         consecutive = 0
         count = 0
-        for col in range(len(self.board[0])):
+        for col in range(self.row_number):
             if self.board[y][col] == num:
                 count += 1
                 consecutive += 1
@@ -153,21 +170,26 @@ class CommandInterface:
                     return False, "three in a row"
             else:
                 consecutive = 0
-        if too_many or count > len(self.board[0]) // 2 + len(self.board[0]) % 2:
+        if count > self.half_row_number_threshold:
             self.board[y][x] = None
             return False, "too many " + str(num)
 
         self.board[y][x] = None
         return True, ""
-    
+
     def is_legal(self, x, y, num):
+
+        # Check if the point is empty
         if self.board[y][x] is not None:
             return False
-        
+
+        # Simulate the move
+        self.board[y][x] = num
+
         consecutive = 0
         count = 0
         self.board[y][x] = num
-        for row in range(len(self.board)):
+        for row in range(self.column_number):
             if self.board[row][x] == num:
                 count += 1
                 consecutive += 1
@@ -176,13 +198,13 @@ class CommandInterface:
                     return False
             else:
                 consecutive = 0
-        if count > len(self.board) // 2 + len(self.board) % 2:
+        if count > self.half_column_number_threshold:
             self.board[y][x] = None
             return False
-        
+
         consecutive = 0
         count = 0
-        for col in range(len(self.board[0])):
+        for col in range(self.row_number):
             if self.board[y][col] == num:
                 count += 1
                 consecutive += 1
@@ -191,18 +213,20 @@ class CommandInterface:
                     return False
             else:
                 consecutive = 0
-        if count > len(self.board[0]) // 2 + len(self.board[0]) % 2:
+        if count > self.half_row_number_threshold:
             self.board[y][x] = None
             return False
 
+        # Undo the move
         self.board[y][x] = None
+
         return True
-    
+
     def valid_move(self, x, y, num):
-        return  x >= 0 and x < len(self.board[0]) and\
-                y >= 0 and y < len(self.board) and\
-                (num == 0 or num == 1) and\
-                self.is_legal(x, y, num)
+        return x >= 0 and x < self.row_number and \
+            y >= 0 and y < self.column_number and \
+            (num == 0 or num == 1) and \
+            self.is_legal(x, y, num)
 
     def play(self, args):
         err = ""
@@ -215,7 +239,7 @@ class CommandInterface:
         except ValueError:
             print("= illegal move: " + " ".join(args) + " wrong coordinate\n")
             return False
-        if  x < 0 or x >= len(self.board[0]) or y < 0 or y >= len(self.board):
+        if x < 0 or x >= self.row_number or y < 0 or y >= self.column_number:
             print("= illegal move: " + " ".join(args) + " wrong coordinate\n")
             return False
         if args[2] != '0' and args[2] != '1':
@@ -232,7 +256,7 @@ class CommandInterface:
         else:
             self.player = 1
         return True
-    
+
     def legal(self, args):
         if not self.arg_check(args, "x y number"):
             return False
@@ -242,14 +266,15 @@ class CommandInterface:
         else:
             print("no")
         return True
-    
+
     def get_legal_moves(self):
         moves = []
-        for y in range(len(self.board)):
-            for x in range(len(self.board[0])):
-                for num in range(2):
-                    if self.is_legal(x, y, num):
-                        moves.append([str(x), str(y), str(num)])
+        for y in range(self.column_number):
+            for x in range(self.row_number):
+                if self.board[y][x] is None:
+                    for num in range(2):
+                        if self.is_legal(x, y, num):
+                            moves.append([str(x), str(y), str(num)])
         return moves
 
     def genmove(self, args):
@@ -257,13 +282,13 @@ class CommandInterface:
         if len(moves) == 0:
             print("resign")
         else:
-            rand_move = moves[random.randint(0, len(moves)-1)]
+            rand_move = moves[random.randint(0, len(moves) - 1)]
             self.play(rand_move)
             print(" ".join(rand_move))
         return True
-    
+
     def winner(self, args):
-        if len(self.get_legal_moves()) == 0:
+        if not self.get_legal_moves():
             if self.player == 1:
                 print(2)
             else:
@@ -271,7 +296,7 @@ class CommandInterface:
         else:
             print("unfinished")
         return True
-    
+
     # new function to be implemented for assignment 2
     def timelimit(self, args):
         self.time_limit = float(args[0])
@@ -279,50 +304,174 @@ class CommandInterface:
 
     # Check if there is a timeout
     def time_out(self):
-        return time.process_time() - self.start_time >= self.time_limit
+        return time.time() - self.start_time >= self.time_limit
 
     # Undo last move
     def undo_move(self, move_played):
         self.board[int(move_played[1])][int(move_played[0])] = None
 
-    # Recursively solve the board using negamax
-    def recursive_solve(self):
+    # Get all the points in the same row of the board
+    def get_row(self, row_index):
+        return self.board[row_index]
 
-        # Check if there is no more move to play
-        if not self.get_legal_moves():
+    # Get all the points in the same column of the board
+    def get_column(self, column_index):
+        return [row[column_index] for row in self.board]
+
+    def categorize_move_deep(self, legal_moves):
+        moves = []
+        categorized_moves = {}
+        number_of_multiple_digit = 0
+        for move in legal_moves:
+            x, y, digit = int(move[0]), int(move[1]), int(move[2])
+            if self.is_legal(x, y, digit):
+                if (x, y) not in categorized_moves:
+                    categorized_moves[(x, y)] = 1
+                else:
+                    number_of_multiple_digit += 1
+                moves.append(move)
+
+        return moves, number_of_multiple_digit
+
+    # Optimization: randomizing the choice of digit of a move
+    # For a move in a row and a column with seemingly equal number of digits,
+    # only 1 digit is randomly chosen for the move
+    def categorize_move_root(self, legal_moves, depth):
+
+        if depth <= 2 and (
+                self.row_number >= 4 and self.column_number >= 4 or self.column_number >= 4 and self.row_number >= 4):
+
+            number_of_multiple_digit = 0
+            moves = []
+            categorized_moves = {}
+            for move in legal_moves:
+                x, y, digit = int(move[0]), int(move[1]), int(move[2])
+                if self.is_legal(x, y, digit):
+                    if (x, y) not in categorized_moves:
+                        categorized_moves[(x, y)] = [digit]
+                    else:
+                        categorized_moves[(x, y)].append(digit)
+
+            for coordinates, digits in categorized_moves.items():
+
+                row = self.get_row(int(coordinates[0]))
+                column = self.get_column(int(coordinates[0]))
+
+                # If both digits can be chosen for this move
+                if len(digits) == 2:
+
+                    zero_row_count = row.count(0)
+                    zero_column_count = column.count(0)
+                    one_row_count = row.count(1)
+                    one_column_count = column.count(1)
+
+                    # If the digits are closely equal in number in a row and in a column
+                    if (((zero_row_count - one_row_count <= 1 or
+                          zero_row_count - one_row_count >= -1) and
+                         zero_row_count + one_row_count < self.half_row_number_threshold) and
+                            ((zero_column_count - one_column_count <= 1 or
+                              zero_column_count - one_column_count >= -1) and
+                             zero_column_count + one_column_count >= self.half_column_number_threshold)):
+                        moves.append([coordinates[0], coordinates[1], digits[random.randint(0, 1)]])
+
+                    # Add the move with distinct digit
+                    else:
+                        moves.append([coordinates[0], coordinates[1], digits[0]])
+
+                    number_of_multiple_digit += 1
+
+                # If only 1 digit can be chosen for this move
+                else:
+                    moves.append([coordinates[0], coordinates[1], digits[0]])
+
+            return moves, number_of_multiple_digit
+
+        else:
+            return self.categorize_move_deep(legal_moves)
+
+    # Recursively solve the board using negamax
+    def recursive_solve(self, moves_to_play, move_sequence, depth):
+
+        # Check if there is only 1 move to play -> Winning
+        if len(moves_to_play) == 1:
+            return True, moves_to_play[0]
+        # Check if there is no more move to play -> Losing
+        elif not moves_to_play:
+            self.positions[move_sequence] = [False, None]
             return False, None
 
         # Check for timeout to cancel all recursive calls
         if self.timed_out:
             return None, None
 
-        # Check if there is still time with the probability of 20% for efficiency
-        if random.random() <= 0.2:
+        # Check if there is still time every 5,000 recursive calls
+        if self.number_of_calls % 5000 == 0:
             if self.time_out():
                 self.timed_out = True
                 return None, None
 
         # Search for a winning move
-        moves = self.get_legal_moves()
+        # Optimize by searching additional level and using the number of points can be played by multiple digits as a heuristic
+        sorted_categorized_moves = []
+        for move in moves_to_play:
+
+            x = int(move[0])
+            y = int(move[1])
+            digit = int(move[2])
+
+            # Check if we encounter this position before to reuse calculations
+            current_position = self.positions.get(move_sequence)
+            if current_position is not None:
+                return current_position[0], current_position[1]
+
+            # Play the move
+            self.board[y][x] = digit
+
+            # Check legal moves to play
+            potential_moves, number_of_multiple_digit = self.categorize_move_root(moves_to_play, depth)
+            sorted_categorized_moves.append((move, number_of_multiple_digit))
+            # Undo the last move to play another move
+            self.board[y][x] = None
+
+        # Optimize by searching moves with a lower number of points having multiple-digit legal move
+        sorted_categorized_moves.sort(key=lambda item: item[1])
+        moves = []
+        for move in sorted_categorized_moves:
+            moves.append(move[0])
+
+        # Search for a winning move
         for move in moves:
-            self.board[int(move[1])][int(move[0])] = int(move[2])
-            winning, winning_move = self.recursive_solve()
-            self.undo_move(move)
+
+            x = int(move[0])
+            y = int(move[1])
+            digit = int(move[2])
+
+            self.board[y][x] = digit
+            winning, winning_move = self.recursive_solve(moves, move_sequence + f"-{x}.{y}.{digit}", depth + 1)
+            self.number_of_calls += 1
+            self.board[y][x] = None
+
+            # If timeout
             if winning is None:
                 return None, None
+
+            # The move is a winning move, the opponent has no winning moves after that
             elif not winning:
+                self.positions[move_sequence] = True, winning_move
                 return True, move
 
+        # If there are no winning moves for us, then we definitely lose
+        self.positions[move_sequence] = False, None
         return False, None
 
     # new function to be implemented for assignment 2
     def solve(self, args):
 
         # Start the timer
-        self.start_time = time.process_time()
+        self.start_time = time.time()
 
         # Begin searching for a winning from the current position
-        winning, winning_move = self.recursive_solve()
+        winning, winning_move = self.recursive_solve(self.get_legal_moves(), "Start", 0)
 
         # There is a timeout event
         if winning is None:
@@ -330,18 +479,20 @@ class CommandInterface:
 
         # The current play is the loser
         elif winning:
-            print(str(self.player) + f" {winning_move[i]}" for i in range(len(winning_move)))
+            print(f"{str(self.player)} {winning_move[0]} {winning_move[1]} {winning_move[2]}")
 
         # The current player is the winner
         else:
             print(3 - self.player)
 
+        # print(f"Time spent: {str(time.time() - self.start_time)}")
         return True
 
-    #===============================================================================================
+    # ===============================================================================================
     # ɅɅɅɅɅɅɅɅɅɅ END OF ASSIGNMENT 2 FUNCTIONS. ɅɅɅɅɅɅɅɅɅɅ
-    #===============================================================================================
-    
+    # ===============================================================================================
+
+
 if __name__ == "__main__":
     interface = CommandInterface()
     interface.main_loop()
